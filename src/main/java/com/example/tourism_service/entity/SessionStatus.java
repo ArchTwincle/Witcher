197b101f10dd3cbd72d7fef73eace32fa9b0027a
ArchTwincle <your-email@example.com>
2026-05-06 22:38:06 +0300

    Remove unrelated Java project files

--- src/main/java/com/example/tourism_service/entity/SessionStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.tourism_service.entity;
-
-public enum SessionStatus {
-    ACTIVE,  // Сессия активна
-    USED,    // Refresh-токен уже был использован для обновления
-    REVOKED  // Сессия отозвана (например, при попытке взлома)
-}
